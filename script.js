46a165e34df9d1de609bf00c74a61d32499b28d1
Dominic Fuentes <militaryforce1@yahoo.com>
2023-07-25 15:47:16 -0500

    explaining logic for begining code

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 // add logic for imperial system 
 // add logic for metric system 
 
-
+// setting up callback function for button
 const convertButton = document.getElementById('convertButton');
     convertButton.addEventListener('click', () => {
+
+        // Gathering user input
         const conversionType = document.getElementById('conversionType').value;
+        const conversionDirection = document.getElementById('conversionDirection').value;
+        
+        // Using parseFloat to ensure the users input is treated as a number
+        // Allows us to preform mathematical calculations correctly  
+        const inputValue = parseFloat(document.getElementById('inputValue').value);
     });
+
+    let result;
+
