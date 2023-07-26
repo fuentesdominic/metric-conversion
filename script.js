d3d55129591fe8ee3c233d7e89f6d4689063000f
Dominic Fuentes <militaryforce1@yahoo.com>
2023-07-26 15:40:09 -0500

    finish structure for both converisons

--- script.js
+++ script.js
@@ -19,102 +19,118 @@ const convertButton_M2I = document.getElementById('convertButton-M2I');
     convertButton_M2I.addEventListener('click', () => {
 
         // Gathering user input
-        // const conversionType = document.getElementById('conversionType').value;
-        // const conversionDirection = document.getElementById('conversionDirection-select').value;
         const metricConversion = document.getElementById('metricConversion').value;
-
-        // const imperialConversion = document.getElementById('imperialConversion').value;
 
         // Using parseFloat to ensure the users input is treated as a number
         // Allows us to preform mathematical calculations correctly  
         const inputValue_M2I = parseFloat(document.getElementById('inputValue-M2I').value);
         
-        let result;
+        // Stores the converted value from the user input
+        let result_M2I;
         
         // Logic for conversion direction
         if (metricConversion) {
             switch (metricConversion) {
                 case 'inchesToCentimeters' :
-                    result = inputValue_M2I * inchesToCentimeters;
+                    result_M2I = inputValue_M2I * inchesToCentimeters;
                     break;
                 case 'feetToMeters' :
-                    result = inputValue_M2I * feetToMeters;
+                    result_M2I = inputValue_M2I * feetToMeters;
                     break;
                 case 'milesToKilometers' :
-                    result = inputValue_M2I * milesToKilometers;
+                    result_M2I = inputValue_M2I * milesToKilometers;
                     break;
                 case 'ouncesToGrams' :
-                    result = inputValue_M2I * ouncesToGrams;
+                    result_M2I = inputValue_M2I * ouncesToGrams;
                     break;
                 case 'poundsToKilograms' : 
-                    result = inputValue_M2I * poundsToKilograms;
+                    result_M2I = inputValue_M2I * poundsToKilograms;
                     break;
                 case 'fluidOuncesToMilliliters' :
                     result = inputValue_M2I * fluidOuncesToMilliliters;
                     break;
                 case 'gallonsToLiters' :
-                    result = inputValue_M2I * gallonsToLiters;
+                    result_M2I = inputValue_M2I * gallonsToLiters;
                     break;
                 case 'fahrenheitToCelsius' :
-                    result = fahrenheitToCelsius(inputValue_M2I);
+                    result_M2I = fahrenheitToCelsius(inputValue_M2I);
                     break;
                 default:
-                    result = 'Invalid Conversion Type';
+                    result_M2I = 'Invalid Conversion Type';
                     break;
-                }
-            } else {
-        // } else if (imperialConversion !== 'imperialConversion' && metricConversion === 'metricConversion') {
-        //     // Imperial to Metric Conversions
-        //     switch(imperialConversion) {
-        //         case 'centimetersToinches' :
-        //             result = inputValue / inchesToCentimeters;
-        //             break;
-        //         case 'metersToFeet' :
-        //             result = inputValue / feetToMeters;
-        //             break;
-        //         case 'kilometersToMiles' :
-        //             result = inputValue / milesToKilometers;
-        //             break;
-        //         case 'gramsToOunces' :
-        //             result = inputValue / ouncesToGrams;
-        //             break;
-        //         case 'kilogramsToPounds' :
-        //             result = inputValue / poundsToKilograms;
-        //             break;
-        //         case 'millilitersToFluidOunces' :
-        //             result = inputValue / fluidOuncesToMilliliters;
-        //             break;
-        //         case 'litersToGallons' :
-        //             result = inputValue / gallonsToLiters;
-        //             break;
-        //         case 'celsiusToFahrenheit' :
-        //             result = (inputValue * 9 / 5) + 32;
-        //             break;
-        //         default :
-        //             result = 'Invalid Conversion Type';
-        //             break;
-        //     }
-        // } else 
-            
-            result = 'Conversion Error';
-        };
+                    }
+                } else {     
+                    result_M2I = 'Conversion Error';
+                };
 
         // Displaying results 
-        if (typeof result !== 'number') {
+        if (typeof result_M2I !== 'number') {
             // handle case where the conversion type is not recongized
-            document.getElementById('result-M2I').textContent = result;
+            document.getElementById('result-M2I').textContent = result_M2I;
         } else {
             // Using toFixed displays the result with decimal places to only two, e.g. "10.88"
-            document.getElementById('result-M2I').textContent = result.toFixed(2);
+            document.getElementById('result-M2I').textContent = result_M2I.toFixed(2);
         };
     
     });
 
-// check why nothing is being displayed when trying to convert.
-
-// see if you can make two seperate switch functions, one for each options and have a button and result text for each selection?
-
 
 // setting up callback function for button
-// const convertButton = document.getElementById('convertButton-I2M');
-//     convertButton_I2M.addEventListener('click', () => {})
+const convertButton_I2M = document.getElementById('convertButton-I2M');
+    convertButton_I2M.addEventListener('click', () => {
+
+        // Gathering user input
+        const imperialConversion = document.getElementById('imperialConversion').value;
+
+        // Using parseFloat to ensure the users input is treated as a number
+        // Allows us to preform mathematical calculations correctly  
+        const inputValue_I2M = parseFloat(document.getElementById('inputValue-I2M').value);
+
+        // Stores the converted value from the user input
+        let result_I2M;
+
+        // Logic for Imperial to Metric conversion
+        if (imperialConversion) {
+            switch (imperialConversion) {
+                case 'centimetersToinches' :
+                    result_I2M = inputValue_I2M / inchesToCentimeters;
+                    break;
+                case 'metersToFeet' :
+                    result_I2M = inputValue_I2M / feetToMeters;
+                    break;
+                case 'kilometersToMiles' :
+                    result_I2M = inputValue_I2M / milesToKilometers;
+                    break;
+                case 'gramsToOunces' :
+                    result_I2M = inputValue_I2M / ouncesToGrams;
+                    break;
+                case 'kilogramsToPounds' :
+                    result_I2M = inputValue_I2M / poundsToKilograms;
+                    break;
+                case 'millilitersToFluidOunces' :
+                    result_I2M = inputValue_I2M / fluidOuncesToMilliliters;
+                    break;
+                case 'litersToGallons' :
+                    result_I2M = inputValue_I2M / gallonsToLiters;
+                    break;
+                case 'celsiusToFahrenheit' :
+                    result_I2M = (inputValue_I2M * 9 / 5) + 32;
+                    break;
+                default :
+                    result_I2M = 'Invalid Conversion Type';
+                    break;
+                    }
+                } else {
+                    result_I2M = 'Conversion Error';
+                };
+
+        // Displaying results
+        if (typeof result_I2M !== 'number') {
+            // handle case where the conversion type is not recongized
+            document.getElementById('result-I2M').textContent = result_I2M;
+        } else {
+            // Using toFixed displays the result with decimal places to only two, e.g. "10.88"
+            document.getElementById('result-I2M').textContent = result_I2M.toFixed(2);
+        }
+
+    });
