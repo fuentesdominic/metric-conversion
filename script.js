696e4133137e1f6d8953bd91292c804ed164cb84
Dominic Fuentes <militaryforce1@yahoo.com>
2023-07-26 15:22:32 -0500

    metric to imperial working

--- script.js
+++ script.js
@@ -15,93 +15,97 @@
     const fahrenheitToCelsius = (fahrenheit) => (fahrenheit - 32) * (5 / 9);
 
 // setting up callback function for button
-const convertButton = document.getElementById('convertButton');
-    convertButton.addEventListener('click', () => {
+const convertButton_M2I = document.getElementById('convertButton-M2I');
+    convertButton_M2I.addEventListener('click', () => {
 
         // Gathering user input
         // const conversionType = document.getElementById('conversionType').value;
         // const conversionDirection = document.getElementById('conversionDirection-select').value;
         const metricConversion = document.getElementById('metricConversion').value;
-        const imperialConversion = document.getElementById('imperialConversion').value;
+
+        // const imperialConversion = document.getElementById('imperialConversion').value;
+
         // Using parseFloat to ensure the users input is treated as a number
         // Allows us to preform mathematical calculations correctly  
-        const inputValue = parseFloat(document.getElementById('inputValue').value);
+        const inputValue_M2I = parseFloat(document.getElementById('inputValue-M2I').value);
         
         let result;
         
         // Logic for conversion direction
-        if (metricConversion !== 'metricConversion' && imperialConversion === 'imperialConversion') {
+        if (metricConversion) {
             switch (metricConversion) {
                 case 'inchesToCentimeters' :
-                    result = inputValue * inchesToCentimeters;
+                    result = inputValue_M2I * inchesToCentimeters;
                     break;
                 case 'feetToMeters' :
-                    result = inputValue * feetToMeters;
+                    result = inputValue_M2I * feetToMeters;
                     break;
                 case 'milesToKilometers' :
-                    result = inputValue * milesToKilometers;
+                    result = inputValue_M2I * milesToKilometers;
                     break;
                 case 'ouncesToGrams' :
-                    result = inputValue * ouncesToGrams;
+                    result = inputValue_M2I * ouncesToGrams;
                     break;
                 case 'poundsToKilograms' : 
-                    result = inputValue * poundsToKilograms;
+                    result = inputValue_M2I * poundsToKilograms;
                     break;
                 case 'fluidOuncesToMilliliters' :
-                    result = inputValue * fluidOuncesToMilliliters;
+                    result = inputValue_M2I * fluidOuncesToMilliliters;
                     break;
                 case 'gallonsToLiters' :
-                    result = inputValue * gallonsToLiters;
+                    result = inputValue_M2I * gallonsToLiters;
                     break;
                 case 'fahrenheitToCelsius' :
-                    result = fahrenheitToCelsius(inputValue);
+                    result = fahrenheitToCelsius(inputValue_M2I);
                     break;
                 default:
                     result = 'Invalid Conversion Type';
                     break;
-            }
-        } else if (imperialConversion !== 'imperialConversion' && metricConversion === 'metricConversion') {
-            // Imperial to Metric Conversions
-            switch(imperialConversion) {
-                case 'centimetersToinches' :
-                    result = inputValue / inchesToCentimeters;
-                    break;
-                case 'metersToFeet' :
-                    result = inputValue / feetToMeters;
-                    break;
-                case 'kilometersToMiles' :
-                    result = inputValue / milesToKilometers;
-                    break;
-                case 'gramsToOunces' :
-                    result = inputValue / ouncesToGrams;
-                    break;
-                case 'kilogramsToPounds' :
-                    result = inputValue / poundsToKilograms;
-                    break;
-                case 'millilitersToFluidOunces' :
-                    result = inputValue / fluidOuncesToMilliliters;
-                    break;
-                case 'litersToGallons' :
-                    result = inputValue / gallonsToLiters;
-                    break;
-                case 'celsiusToFahrenheit' :
-                    result = (inputValue * 9 / 5) + 32;
-                    break;
-                default :
-                    result = 'Invalid Conversion Type';
-                    break;
-            }
-        } else {
-            result = 'Please select either Metric to Imperial or Imperial to Metric conversion.';
-        }
+                }
+            } else {
+        // } else if (imperialConversion !== 'imperialConversion' && metricConversion === 'metricConversion') {
+        //     // Imperial to Metric Conversions
+        //     switch(imperialConversion) {
+        //         case 'centimetersToinches' :
+        //             result = inputValue / inchesToCentimeters;
+        //             break;
+        //         case 'metersToFeet' :
+        //             result = inputValue / feetToMeters;
+        //             break;
+        //         case 'kilometersToMiles' :
+        //             result = inputValue / milesToKilometers;
+        //             break;
+        //         case 'gramsToOunces' :
+        //             result = inputValue / ouncesToGrams;
+        //             break;
+        //         case 'kilogramsToPounds' :
+        //             result = inputValue / poundsToKilograms;
+        //             break;
+        //         case 'millilitersToFluidOunces' :
+        //             result = inputValue / fluidOuncesToMilliliters;
+        //             break;
+        //         case 'litersToGallons' :
+        //             result = inputValue / gallonsToLiters;
+        //             break;
+        //         case 'celsiusToFahrenheit' :
+        //             result = (inputValue * 9 / 5) + 32;
+        //             break;
+        //         default :
+        //             result = 'Invalid Conversion Type';
+        //             break;
+        //     }
+        // } else 
+            
+            result = 'Conversion Error';
+        };
 
         // Displaying results 
         if (typeof result !== 'number') {
             // handle case where the conversion type is not recongized
-            document.getElementById('result').textContent = result;
+            document.getElementById('result-M2I').textContent = result;
         } else {
             // Using toFixed displays the result with decimal places to only two, e.g. "10.88"
-            document.getElementById('result').textContent = result.toFixed(2);
+            document.getElementById('result-M2I').textContent = result.toFixed(2);
         };
     
     });
@@ -109,3 +113,8 @@ const convertButton = document.getElementById('convertButton');
 // check why nothing is being displayed when trying to convert.
 
 // see if you can make two seperate switch functions, one for each options and have a button and result text for each selection?
+
+
+// setting up callback function for button
+// const convertButton = document.getElementById('convertButton-I2M');
+//     convertButton_I2M.addEventListener('click', () => {})
